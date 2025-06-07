80635ffa2c196875d865f7331e51543a5c033b67
yuto82 <rgritsal@gmail.com>
2025-06-07 23:20:20 +0200

    update: fetching data from spotify

--- etl/config.py
+++ etl/config.py
@@ -1,39 +1,10 @@
 import os
-import base64
-import requests
-import webbrowser
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 
 path_dotenv = find_dotenv()
 load_dotenv(path_dotenv)
-
 class Config:
     CLIENT_ID = os.getenv("CLIENT_ID")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     REDIRECT_URI = os.getenv("REDIRECT_URI")
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-
-# def access_token():
-#     token_url = "https://accounts.spotify.com/api/token"
-
-#     auth_header = base64.b64encode(f"{Config.CLIENT_ID}:{Config.CLIENT_SECRET}".encode()).decode()
-#     headers = {
-#         "Authorization": f"Basic {auth_header}",
-#         "Content-Type": "application/x-www-form-urlencoded"
-#     }
-
-#     data = {
-#         "grant_type": "authorization_code",
-#         "code": Config.CODE,
-#         "redirect_uri": Config.REDIRECT_URI
-#     }
-
-#     response = requests.post(token_url, data=data, headers=headers)
-#     tokens = response.json()
-#     print("Access Token:", tokens.get("access_token"))
-#     print("Refresh Token:", tokens.get("refresh_token"))
-#     print("Expires In:", tokens.get("expires_in"))
-
--- etl/extract.py
+++ etl/extract.py
@@ -1,12 +1,17 @@
-import requests
-from config import Config
+import json
+from callback_handler import spotify_authorize, access_token, get_recent_tracks
 
-def get_recent_tracks(access_token=Config.ACCESS_TOKEN):
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    url = "https://api.spotify.com/v1/me/player/recently-played?limit=50"
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    data = response.json()
-    print(data["items"])
+def main():
+    code = spotify_authorize()
+
+    acess_token = access_token(code)
+    
+    spotify_data = get_recent_tracks(acess_token)
+    
+
+    spotify_file = "spotify_data.json"
+    with open(spotify_file, "w") as file:
+            json.dump(spotify_data, file, indent=4)
+
+if __name__ == "__main__":
+    main()
